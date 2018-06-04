dfae208f61f798598145e13179ec549f2e557850
mattos <oliverscheduler@gmail.com>
2018-06-04 19:54:45 +0800

    update cpython to 3.6.5

--- src/lib/python/Programs/_testembed.c
+++ src/lib/python/Programs/_testembed.c
@@ -1,297 +1,198 @@
 #include <Python.h>
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
+#include "pythread.h"
+#include <stdio.h>
 
-#include <chrono>
-#include <thread>
+/*********************************************************
+ * Embedded interpreter tests that need a custom exe
+ *
+ * Executed via 'EmbeddingTests' in Lib/test/test_capi.py
+ *********************************************************/
 
-#include <Python.h>
-#include "structmember.h"
-
-#define SCRIPT_PYTH \
-			L"../Lib:"\
-
-typedef struct {
-	PyObject_HEAD
-		PyObject *first;
-	PyObject *last;
-	int number;
-} Noddy;
-
-static int
-Noddy_traverse(Noddy *self, visitproc visit, void *arg)
+static void _testembed_Py_Initialize(void)
 {
-	int vret;
-
-	if (self->first) {
-		vret = visit(self->first, arg);
-		if (vret != 0)
-			return vret;
-	}
-	if (self->last) {
-		vret = visit(self->last, arg);
-		if (vret != 0)
-			return vret;
-	}
-
-	return 0;
-}
-
-static int
-Noddy_clear(Noddy *self)
-{
-	PyObject *tmp;
-
-	tmp = self->first;
-	self->first = NULL;
-	Py_XDECREF(tmp);
-
-	tmp = self->last;
-	self->last = NULL;
-	Py_XDECREF(tmp);
-
-	return 0;
-}
-
-static void
-Noddy_dealloc(Noddy* self)
-{
-	PyObject_GC_UnTrack(self);
-	Noddy_clear(self);
-	Py_TYPE(self)->tp_free((PyObject*)self);
-}
-
-static PyObject *
-Noddy_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
-{
-	Noddy *self;
-
-	self = (Noddy *)type->tp_alloc(type, 0);
-	if (self != NULL) {
-		self->first = PyUnicode_FromString("");
-		if (self->first == NULL) {
-			Py_DECREF(self);
-			return NULL;
-		}
-
-		self->last = PyUnicode_FromString("");
-		if (self->last == NULL) {
-			Py_DECREF(self);
-			return NULL;
-		}
-
-		self->number = 0;
-	}
-
-	return (PyObject *)self;
-}
-
-static int
-Noddy_init(Noddy *self, PyObject *args, PyObject *kwds)
-{
-	PyObject *first = NULL, *last = NULL, *tmp;
-
-	static char *kwlist[] = { "first", "last", "number", NULL };
-
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OOi", kwlist,
-		&first, &last,
-		&self->number))
-		return -1;
-
-	if (first) {
-		tmp = self->first;
-		Py_INCREF(first);
-		self->first = first;
-		Py_XDECREF(tmp);
-	}
-
-	if (last) {
-		tmp = self->last;
-		Py_INCREF(last);
-		self->last = last;
-		Py_XDECREF(tmp);
-	}
-
-	return 0;
+    /* HACK: the "./" at front avoids a search along the PATH in
+       Modules/getpath.c */
+    Py_SetProgramName(L"./_testembed");
+    Py_Initialize();
 }
 
 
-static PyMemberDef Noddy_members[] = {
-	{ "first", T_OBJECT_EX, offsetof(Noddy, first), 0,
-	"first name" },
-	{ "last", T_OBJECT_EX, offsetof(Noddy, last), 0,
-	"last name" },
-	{ "number", T_INT, offsetof(Noddy, number), 0,
-	"noddy number" },
-	{ NULL }  /* Sentinel */
+/*****************************************************
+ * Test repeated initialisation and subinterpreters
+ *****************************************************/
+
+static void print_subinterp(void)
+{
+    /* Just output some debug stuff */
+    PyThreadState *ts = PyThreadState_Get();
+    printf("interp %p, thread state %p: ", ts->interp, ts);
+    fflush(stdout);
+    PyRun_SimpleString(
+        "import sys;"
+        "print('id(modules) =', id(sys.modules));"
+        "sys.stdout.flush()"
+    );
+}
+
+static int test_repeated_init_and_subinterpreters(void)
+{
+    PyThreadState *mainstate, *substate;
+#ifdef WITH_THREAD
+    PyGILState_STATE gilstate;
+#endif
+    int i, j;
+
+    for (i=0; i<15; i++) {
+        printf("--- Pass %d ---\n", i);
+        _testembed_Py_Initialize();
+        mainstate = PyThreadState_Get();
+
+#ifdef WITH_THREAD
+        PyEval_InitThreads();
+        PyEval_ReleaseThread(mainstate);
+
+        gilstate = PyGILState_Ensure();
+#endif
+        print_subinterp();
+        PyThreadState_Swap(NULL);
+
+        for (j=0; j<3; j++) {
+            substate = Py_NewInterpreter();
+            print_subinterp();
+            Py_EndInterpreter(substate);
+        }
+
+        PyThreadState_Swap(mainstate);
+        print_subinterp();
+#ifdef WITH_THREAD
+        PyGILState_Release(gilstate);
+#endif
+
+        PyEval_RestoreThread(mainstate);
+        Py_Finalize();
+    }
+    return 0;
+}
+
+/*****************************************************
+ * Test forcing a particular IO encoding
+ *****************************************************/
+
+static void check_stdio_details(const char *encoding, const char * errors)
+{
+    /* Output info for the test case to check */
+    if (encoding) {
+        printf("Expected encoding: %s\n", encoding);
+    } else {
+        printf("Expected encoding: default\n");
+    }
+    if (errors) {
+        printf("Expected errors: %s\n", errors);
+    } else {
+        printf("Expected errors: default\n");
+    }
+    fflush(stdout);
+    /* Force the given IO encoding */
+    Py_SetStandardStreamEncoding(encoding, errors);
+    _testembed_Py_Initialize();
+    PyRun_SimpleString(
+        "import sys;"
+        "print('stdin: {0.encoding}:{0.errors}'.format(sys.stdin));"
+        "print('stdout: {0.encoding}:{0.errors}'.format(sys.stdout));"
+        "print('stderr: {0.encoding}:{0.errors}'.format(sys.stderr));"
+        "sys.stdout.flush()"
+    );
+    Py_Finalize();
+}
+
+static int test_forced_io_encoding(void)
+{
+    /* Check various combinations */
+    printf("--- Use defaults ---\n");
+    check_stdio_details(NULL, NULL);
+    printf("--- Set errors only ---\n");
+    check_stdio_details(NULL, "ignore");
+    printf("--- Set encoding only ---\n");
+    check_stdio_details("latin-1", NULL);
+    printf("--- Set encoding and errors ---\n");
+    check_stdio_details("latin-1", "replace");
+
+    /* Check calling after initialization fails */
+    Py_Initialize();
+
+    if (Py_SetStandardStreamEncoding(NULL, NULL) == 0) {
+        printf("Unexpected success calling Py_SetStandardStreamEncoding");
+    }
+    Py_Finalize();
+    return 0;
+}
+
+
+/*********************************************************
+ * Test parts of the C-API that work before initialization
+ *********************************************************/
+
+static int test_pre_initialization_api(void)
+{
+    /* Leading "./" ensures getpath.c can still find the standard library */
+    wchar_t *program = Py_DecodeLocale("./spam", NULL);
+    if (program == NULL) {
+        fprintf(stderr, "Fatal error: cannot decode program name\n");
+        return 1;
+    }
+    Py_SetProgramName(program);
+
+    Py_Initialize();
+    Py_Finalize();
+
+    PyMem_RawFree(program);
+    return 0;
+}
+
+
+/* *********************************************************
+ * List of test cases and the function that implements it.
+ *
+ * Names are compared case-sensitively with the first
+ * argument. If no match is found, or no first argument was
+ * provided, the names of all test cases are printed and
+ * the exit code will be -1.
+ *
+ * The int returned from test functions is used as the exit
+ * code, and test_capi treats all non-zero exit codes as a
+ * failed test.
+ *********************************************************/
+struct TestCase
+{
+    const char *name;
+    int (*func)(void);
 };
 
-static PyObject *
-Noddy_name(Noddy* self)
-{
-	if (self->first == NULL) {
-		PyErr_SetString(PyExc_AttributeError, "first");
-		return NULL;
-	}
-
-	if (self->last == NULL) {
-		PyErr_SetString(PyExc_AttributeError, "last");
-		return NULL;
-	}
-
-	return PyUnicode_FromFormat("%S %S", self->first, self->last);
-}
-
-static PyObject *
-Noddy_sendMessage(Noddy* self, PyObject *args, PyObject *kwds)
-{
-	PyObject *methodName = NULL, *methodArgs = NULL;
-
-	static char *kwlist[] = { "methodPort", "methodName", "methodArgs", NULL };
-
-	if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OOO", kwlist, &methodName, &methodArgs))
-		return NULL;
-
-	if (methodName) {
-		Py_INCREF(methodName);
-	}
-
-	if (methodArgs) {
-		Py_INCREF(methodArgs);
-	}
-
-	return NULL;
-}
-
-
-static PyMethodDef Noddy_methods[] = {
-	{ "name", (PyCFunction)Noddy_name, METH_NOARGS,
-	"Return the name, combining the first and last name"
-	},
-	{ NULL }  /* Sentinel */
+static struct TestCase TestCases[] = {
+    { "forced_io_encoding", test_forced_io_encoding },
+    { "repeated_init_and_subinterpreters", test_repeated_init_and_subinterpreters },
+    { "pre_initialization_api", test_pre_initialization_api },
+    { NULL, NULL }
 };
 
-static PyTypeObject NoddyType = {
-	PyVarObject_HEAD_INIT(NULL, 0)
-	"noddy.Noddy",             /* tp_name */
-	sizeof(Noddy),             /* tp_basicsize */
-	0,                         /* tp_itemsize */
-	(destructor)Noddy_dealloc, /* tp_dealloc */
-	0,                         /* tp_print */
-	0,                         /* tp_getattr */
-	0,                         /* tp_setattr */
-	0,                         /* tp_reserved */
-	0,                         /* tp_repr */
-	0,                         /* tp_as_number */
-	0,                         /* tp_as_sequence */
-	0,                         /* tp_as_mapping */
-	0,                         /* tp_hash  */
-	0,                         /* tp_call */
-	0,                         /* tp_str */
-	0,                         /* tp_getattro */
-	0,                         /* tp_setattro */
-	0,                         /* tp_as_buffer */
-	Py_TPFLAGS_DEFAULT |
-	Py_TPFLAGS_BASETYPE |
-	Py_TPFLAGS_HAVE_GC,    /* tp_flags */
-	"Noddy objects",           /* tp_doc */
-	(traverseproc)Noddy_traverse,   /* tp_traverse */
-	(inquiry)Noddy_clear,           /* tp_clear */
-	0,                         /* tp_richcompare */
-	0,                         /* tp_weaklistoffset */
-	0,                         /* tp_iter */
-	0,                         /* tp_iternext */
-	Noddy_methods,             /* tp_methods */
-	Noddy_members,             /* tp_members */
-	0,                         /* tp_getset */
-	0,                         /* tp_base */
-	0,                         /* tp_dict */
-	0,                         /* tp_descr_get */
-	0,                         /* tp_descr_set */
-	0,                         /* tp_dictoffset */
-	(initproc)Noddy_init,      /* tp_init */
-	0,                         /* tp_alloc */
-	Noddy_new,                 /* tp_new */
-};
-
-void enterLoop(PyObject* entryScript) {
-	for (;;)
-	{
-		PyObject* pyResult = PyObject_CallMethod(entryScript, "onTimerUpdate", "l", time(NULL));
-		std::this_thread::sleep_for(std::chrono::seconds(5));
-		if (pyResult != NULL)
-		{
-			Py_DECREF(pyResult);
-		}
-		else {
-			PyErr_PrintEx(0);
-			break;
-		}
-	}
-
-}
-
-/* Different embedding tests */
 int main(int argc, char *argv[])
 {
-	// Py_SetPythonHome(L"/home/mattos/tutorial/tutorial/");
-	// Initialise python
-	// Py_VerboseFlag = 2;
-	Py_FrozenFlag = 1;
-	// Warn if tab and spaces are mixed in indentation.
-	// Py_TabcheckFlag = 1;
-	Py_NoSiteFlag = 1;
-	// Py_IgnoreEnvironmentFlag = 1;
-	Py_SetProgramName(L"MyApp");
+    if (argc > 1) {
+        for (struct TestCase *tc = TestCases; tc && tc->name; tc++) {
+            if (strcmp(argv[1], tc->name) == 0)
+                return (*tc->func)();
+        }
+    }
 
-	Py_Initialize();
-	if (!Py_IsInitialized())
-	{
-		std::cout << "Script::install(): Py_Initialize is failed!\n";
-		return 1;
-	}
-	// const wchar_t * pathStr = L"../Lib:../build/lib.linux-x86_64-3.6";
-	// PySys_SetPath(pathStr);
+    /* No match found, or no test name provided, so display usage */
+    printf("Python " PY_VERSION " _testembed executable for embedded interpreter tests\n"
+           "Normally executed via 'EmbeddingTests' in Lib/test/test_capi.py\n\n"
+           "Usage: %s TESTNAME\n\nAll available tests:\n", argv[0]);
+    for (struct TestCase *tc = TestCases; tc && tc->name; tc++) {
+        printf("  %s\n", tc->name);
+    }
 
-	PyObject *mMain = PyImport_AddModule("__main__");
-	PyObject* newModule = PyImport_AddModule("noddy4");
-	if (PyType_Ready(&NoddyType) < 0)
-		return 1;
-
-	Py_INCREF(&NoddyType);
-
-	PyModule_AddObject(newModule, "Noddy", (PyObject *)&NoddyType);
-
-	PyRun_SimpleString(
-		"import sys;"
-		"print('stdin: {0.encoding}:{0.errors}'.format(sys.stdin));"
-		"print('stdout: {0.encoding}:{0.errors}'.format(sys.stdout));"
-		"print('stderr: {0.encoding}:{0.errors}'.format(sys.stderr));"
-		"sys.path.append('tutorial');"
-		"print(sys.path);"
-		"from pathlib import Path;"
-		"print('home: {}'.format(str(Path.home())));"
-		"sys.stdout.flush();"
-	);
-
-	PyObject *pyEntryScriptFileName = PyUnicode_FromString("game");
-
-	PyObject* entryScript = PyImport_Import(pyEntryScriptFileName);
-
-	if (entryScript == NULL) {
-		PyErr_PrintEx(0);
-		return 1;
-	}
-
-	PyObject* pyResult = PyObject_CallMethod(entryScript, "onMyAppRun", "O", PyBool_FromLong(1));
-	if (pyResult != NULL)
-		Py_DECREF(pyResult);
-	else
-		PyErr_PrintEx(0);
-
-	enterLoop(entryScript);
-	std::this_thread::sleep_for(std::chrono::seconds(3));
-	Py_Finalize();
-	return 0;
+    /* Non-zero exit code will cause test_capi.py tests to fail.
+       This is intentional. */
+    return -1;
 }
